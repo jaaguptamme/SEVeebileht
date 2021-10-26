71c3db4ae3b1b59467bf89ebafa306d27dbfa42e
Jaagup Tamme <jaaguptamme@gmail.com>
2021-10-26 17:23:36 +0300

    otsingu kommenteerimine

--- koodid/otsing.js
+++ koodid/otsing.js
@@ -6,15 +6,14 @@ for(let i=0;i<nimed.length;i++){
 }
 
 function LaeTabel(){//funktsioon, mis genereerib html-i, mida kasutajale näidatakse
-    
+    var uusHtml="";//genereerime uue html-koodi
     var read=andmed.split('\n');//jagab failis andmed.js antud string tüüpi muutuja ridadeks
     for(let i=0;i<read.length;i++){
-        var vaartused=read[i].split("'");
+        var vaartused=read[i].split("'");//jaotame iga rea veergudeks, seda raskenavad erinevat tüüpi positaadressid
         var v2=[];
         for(let j=0;j<vaartused.length;j++){
             if(j%2==0){
                 var veerud=vaartused[j].split(',');
-
                 for(let k=(j==0?0:1);k<veerud.length-1;k++){
                     v2.push(veerud[k]);
                 }
@@ -23,17 +22,17 @@ function LaeTabel(){//funktsioon, mis genereerib html-i, mida kasutajale näidat
                 v2.push(vaartused[j]);
             }
         }
-        vaartused=v2;
+        vaartused=v2;//kõik veerud on nüüd eraldi elemendid järjendis
 
-        var uusRida="<tr>";
+        var uusRida="<tr>";//loome uue rea tabelisse
         for(let j=0;j<vaartused.length;j++){
-            uusRida+="<td class='"+tahendused[j]+"'>";
-            if(tahendused[j]=="koduleht" && vaartused[j].includes("http")){
+            uusRida+="<td class='"+tahendused[j]+"'>";//iga elemendi klassiks saab tema kohal päises olev sõna
+            if(tahendused[j]=="koduleht" && vaartused[j].includes("http")){//kodulehtedele on klikitavad lingid
                 uusRida+="<a href='"+vaartused[j]+"'>";
                 uusRida+=vaartused[j];
                 uusRida+="</a>"
             }
-            else if(tahendused[j]=="email" && vaartused[j].includes("@")){
+            else if(tahendused[j]=="email" && vaartused[j].includes("@")){//emaili aadressitele loome samuti lingid
                 uusRida+="<a href='mailto:"+vaartused[j]+"'>";
                 uusRida+=vaartused[j];
                 uusRida+="</a>"
@@ -44,6 +43,8 @@ function LaeTabel(){//funktsioon, mis genereerib html-i, mida kasutajale näidat
             uusRida+="</td>";
         }
         uusRida+="</tr>";
-        document.getElementById("p6hitabel").getElementsByTagName('tbody')[0].innerHTML+=uusRida;
+        uusHtml+=uusRida;
     }
+    /*uuendame lehe html-koodi*/
+    document.getElementById("p6hitabel").getElementsByTagName('tbody')[0].innerHTML=uusHtml;
 }
