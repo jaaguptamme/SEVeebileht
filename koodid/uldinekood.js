f95ce22fd49cf382a2c5854888927807d66a0db7
Jaagup Tamme <jaaguptamme@gmail.com>
2021-10-28 17:04:12 +0300

    Kontakti vahelehe lisamine

--- koodid/uldinekood.js
+++ koodid/uldinekood.js
@@ -3,4 +3,5 @@
 let nuppudehtml="";
 nuppudehtml+=`<input class="algusnupp" id="avalehele" type="button" onclick="location.href='Index.html'" value="Avalehele"/>`
 nuppudehtml+=`<input class="algusnupp" id="otsingule" type="button" onclick="location.href='Otsing.html'" value="Otsingumootor"/>`
+nuppudehtml+=`<input class="algusnupp" id="kontakt" type="button" onclick="location.href='Kontakt.html'" value="Kontakteeru meiega"/>`
 document.getElementById("päis").innerHTML+=nuppudehtml;
