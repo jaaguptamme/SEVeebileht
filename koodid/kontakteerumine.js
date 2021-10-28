d373889cc786339fadb6ab1cd9be6ecd185ea9cd
Jaagup Tamme <jaaguptamme@gmail.com>
2021-10-28 18:05:50 +0300

    kontakteerumise vahelehe täiustamine

---
+++ koodid/kontakteerumine.js
@@ -0,0 +1,17 @@
+function Saada(){//kutsutakse välja kui kasutaja üritab andmeid edastada
+    /*if(document.getElementById("nimi").value.length<5){//kasutaja pole nime sisetanud
+        alert("Sisesta palun oma nimi");
+        return;
+    }
+    if(document.getElementById("telefon").value.length<5&&
+    (document.getElementById("email").value.length<5 || document.getElementById("email").value.includes("@")==0)){
+        //kasutaja peab sisestama telefoni või oma e-postiaadressi
+        alert("Siseta palun oma telefoninumber või e-posti aadress");
+        return;
+    }
+    if(document.getElementById("kirjeldus").value.length<20){//kasutaja ei kirjutanud koha kohta peaaegu midagi
+        alert("Siesta palun koha kirjeldus");
+        return;
+    }*/
+    document.getElementById("pealesisestamist").style.display="block";
+}
