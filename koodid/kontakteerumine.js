b8643784206ee72db91b14e64a3218c0408edd5f
Jaagup Tamme <jaaguptamme@gmail.com>
2021-10-28 18:46:23 +0300

    ekirja koostamise loogika loomine

--- koodid/kontakteerumine.js
+++ koodid/kontakteerumine.js
@@ -13,18 +13,34 @@ function SaadaKontrolli(){//kutsutakse välja kui kasutaja üritab andmeid edast
         alert("Siesta palun koha kirjeldus");
         return;
     }*/
+
     document.getElementById("pealesisestamist").style.display="block";
+    document.getElementById("saatmisnupp").style.display="none";
 }
 function TäiendanVeel(){//kasutaja täiendab veel sisestatavaid andmeid.
     document.getElementById("pealesisestamist").style.display="none";
+    document.getElementById("saatmisnupp").style.display="block";
 }
 function SaadaLõplikult(){//kasutaja saadab andmed lõplikult ära;
-    document.getElementById("pealesisestamist").style.display="none";
-    window.open("mailto:test@example.com?subject=&body=");
 
+    document.getElementById("pealesisestamist").style.display="none";
+
+    /*ekirja koostamine, need imelikud sümbolid on reavahetuse tähistamiseks*/
+    let sõnum="Hei, olen "+document.getElementById("nimi").value +" ja haldan ühte lahedat kohta. Soovin, et see oleks leitav sinu veebilehel. %0d%0a";
+    sõnum+="Koha lühikirjeldus: "+document.getElementById("kirjeldus").value +"%0d%0a";
+    sõnum+="Minu kontakt andmed: %0d%0a";
+    sõnum+="Telefoninumber: "+document.getElementById("telefon").value+"%0d%0a";
+    sõnum+="E-posti aadress: "+document.getElementById("email").value+"%0d%0a";
+    sõnum+="%0d%0a Heade soovidega, %0d%0a "+document.getElementById("nimi").value;
+
+    /*ekirja saatmine*/
+    window.open("mailto:test@example.com?subject=Haldan ägedat kohta ja soovin selle sinu veebilehe üles panna!&body="+sõnum);
+
+    let väljad=["nimi","telefon","email","kirjeldus"];//andmeväljad
     /*tühjenda andmeväljad*/
-    let väljad=["nimi","telefon","email","kirjeldus"];
     for(let i=0;i<väljad.length;i++){
         document.getElementById(väljad[i]).value="";
     }
+    /*Avame kasutajale tänusõnad*/
+    document.getElementById("tänusõnad").style.display="block";
 }
