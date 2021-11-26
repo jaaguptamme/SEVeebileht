6fb23301948adf0d6017cb68e586c2a3dce828fe
Marilin Ahvenainen <Marilin@Juhans-MBP.lan>
2021-11-26 19:59:02 +0200

    esilehe scroll

--- koodid/avaleht.js
+++ koodid/avaleht.js
@@ -1,3 +1,12 @@
 /*
     Selles failis on avalehe kood.
 */
+/*
+  juhend: https://codetea.com/css-scroll-down-button/
+*/
+$(function() {
+  $('a[href*=#]').on('click', function(e) { // kui vastavale elemendile vajutatakse siis funktsioon kutsutakse välja
+    e.preventDefault(); // väldib vaikimisi klõpsu
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear'); // määrab kerimisele vastavad parameetrid näiteks millisekundid kaua kerimisele kulub.
+  });
+});
